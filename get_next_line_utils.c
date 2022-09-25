1216400fd3e923e1aa683fe150cdc635387e09fe
Douglas Fanucchi <douglasfanucchi@gmail.com>
2022-09-25 12:45:11 -0300

    add utils file

---
+++ get_next_line_utils.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfanucch <dfanucch@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/25 12:35:31 by dfanucch          #+#    #+#             */
+/*   Updated: 2022/09/25 12:35:31 by dfanucch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	size;
+
+	size = 0;
+	while (*str++)
+		size++;
+	return (size);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s && *s != (unsigned char) c)
+		s++;
+	if (*s == (const char) c)
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strdup(const char *s)
+{
+	size_t	len;
+	char	*cpy;
+
+	len = ft_strlen(s) + 1;
+	cpy = malloc(len * sizeof(char));
+	if (cpy)
+		ft_strlcpy(cpy, s, len);
+	return (cpy);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*substr;
+	size_t	substr_len;
+
+	substr_len = 0;
+	if (start >= ft_strlen(s))
+		len = 0;
+	while (substr_len < len && s[substr_len + start])
+		substr_len++;
+	substr = (char *)malloc((substr_len + 1) * sizeof(char));
+	if (substr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < substr_len && s[start])
+		substr[i++] = s[start++];
+	substr[i] = 0;
+	return (substr);
+}
